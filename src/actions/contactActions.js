4e087b7840f4d193115924621fc5e21d582c5676
sliim35 <slim.r35@gmail.com>
2018-08-10 17:30:33 +0300

    first commit

---
+++ src/actions/contactActions.js
@@ -0,0 +1,32 @@
+import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from './types';
+import axios from 'axios';
+
+export const getContacts = () => async dispatch => {
+  const res = await axios.get('https://jsonplaceholder.typicode.com/users');
+
+  dispatch({
+    type: GET_CONTACTS,
+    payload: res.data
+  });
+};
+
+export const addContact = contact => async dispatch => {
+  const res = await axios.post(
+    'https://jsonplaceholder.typicode.com/users',
+    contact
+  );
+
+  dispatch({
+    type: ADD_CONTACT,
+    payload: res.data
+  });
+};
+
+export const deleteContact = id => async dispatch => {
+  await axios.delete(`https://jsonplaceholder.typicode.com/users/${id}`);
+
+  dispatch({
+    type: DELETE_CONTACT,
+    payload: id
+  });
+};
---
+++ src/reducers/contactReducer.js
@@ -0,0 +1,29 @@
+import { GET_CONTACTS, ADD_CONTACT, DELETE_CONTACT } from '../actions/types';
+
+const initialState = {
+  contacts: []
+};
+
+export default function(state = initialState, action) {
+  switch (action.type) {
+    case GET_CONTACTS:
+      return {
+        ...state,
+        contacts: action.payload
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          contact => contact.id !== action.payload
+        )
+      };
+    case ADD_CONTACT:
+      return {
+        ...state,
+        contacts: [action.payload, ...state.contacts]
+      };
+    default:
+      return state;
+  }
+}
