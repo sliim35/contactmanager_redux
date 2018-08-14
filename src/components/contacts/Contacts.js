d0a59e6115662688c522c3870e7783404ef6b315
sliim35 <slim.r35@gmail.com>
2018-08-14 14:24:45 +0300

    Prepare to deploy

--- src/components/contacts/Contacts.js
+++ src/components/contacts/Contacts.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Contact from './Contact';
 import { connect } from 'react-redux';
-import PropTypes from 'prop-types';
 import { getContacts } from '../../actions/contactActions';
 
 class Contacts extends Component {
